bfddde2552e89a32af527e77c2edc572dd3e8507
N.samimian <41186578+nrmiin@users.noreply.github.com>
2019-01-28 16:01:16 +0330

    Update Main.ino

--- NIOTKit/examples/Main/Main.ino
+++ NIOTKit/examples/Main/Main.ino
@@ -8,7 +8,8 @@ NioKit Dataset( "Niomatic_AP","12345678",IPAddress(192,168,1,36),48000,"AP");
 
 void setup()
 {  
-  //Dataset.pubnub("pubsub.pubnub.com", "pub-c-c5e5a0e5-37e3-4e64-8930-a1b75e81941f", "sub-c-db676276-f860-11e8-9488-9e80330262eb", "demo");
+  //in order to use pubnub servers connect to internet(WifiMode-->STA) and enter your pubnub publishkey and subscribekey below. 
+  //Dataset.pubnub("pubsub.pubnub.com", "publishkey", "subscribekey", "channel name");      //uncomment for pubnub service
   //Dataset.SetupDht(pin);          //uncomment for DHT Sensor and set the pin
   //Dataset.SetupRgb(pin);          //uncomment for RGB and set the pin
   //Dataset.SetupBuzzer(pin);       //uncomment for Buzzer and set the pin
@@ -19,10 +20,9 @@ void setup()
   //Dataset.SetupAnalog(A0);        //uncomment for analog Sensor and connect it to A0
   //Dataset.SetupVoltmeter(A0);     //uncomment for Voltage Sensor and connect it to A0
   //Dataset.SetupLdr(A0);           //uncomment for LDR Sensor and connect it to A0
-  //Dataset.SetupLiqlevel(A0);      //uncomment for LIquidLevel Sensor and connect it to A0
+  //Dataset.SetupLiqlevel(A0);      //uncomment for LiquidLevel Sensor and connect it to A0
   //Dataset.SetupMoisture(A0);      //uncomment for Moisture Sensor and connect it to A0
   //Dataset.SetupMq5(A0);           //uncomment for Gas sensor connect it to A0
-
 }
 
 void loop()
